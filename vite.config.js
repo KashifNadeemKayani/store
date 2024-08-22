2c32431114325971af0ace5bfa2197d57e7be972
KashifNadeemKayani <KAYANIKASHIF970@GMAIL.COM>
2024-08-22 23:05:15 +0500

    Rebuild and deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,4 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/store/',  // Replace 'store' with your actual repository name if it's different
-})
+export default {
+  base: '/store/', // replace 'myCV' with your repository name
+}
